7936279b9405fa2020598bb942d72cccc032a78a
Ana Cidre <ana@anacidre.com>
2018-05-15 10:19:45 +0200

    Cache images too

--- sw.js
+++ sw.js
@@ -11,7 +11,18 @@ self.addEventListener('install', function(event) {
                 'js/main.js',
                 'js/restaurant_info.js',
                 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
-                'https://fonts.gstatic.com/s/roboto/v18/KFOlCnqEu92Fr1MmEU9fBBc4AMP6lQ.woff2'
+                'https://fonts.gstatic.com/s/roboto/v18/KFOlCnqEu92Fr1MmEU9fBBc4AMP6lQ.woff2',
+                'data/restaurants.json',
+                'img/1.jpg',
+                'img/2.jpg',
+                'img/3.jpg',
+                'img/4.jpg',
+                'img/5.jpg',
+                'img/6.jpg',
+                'img/7.jpg',
+                'img/8.jpg',
+                'img/9.jpg',
+                'img/10.jpg'
             ]);
         })
     );
