c66366793c7862d33766b7d377b696d95cd01b71
Ana Cidre <ana@anacidre.com>
2018-05-24 13:42:58 +0200

    Update caching to add images

--- sw.js
+++ sw.js
@@ -10,9 +10,6 @@ self.addEventListener('install', function(event) {
                 'js/index.js',
                 'js/main.js',
                 'js/restaurant_info.js',
-                'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
-                'https://fonts.gstatic.com/s/roboto/v18/KFOlCnqEu92Fr1MmEU9fBBc4AMP6lQ.woff2',
-                'data/restaurants.json',
                 'img/1.jpg',
                 'img/2.jpg',
                 'img/3.jpg',
@@ -22,7 +19,10 @@ self.addEventListener('install', function(event) {
                 'img/7.jpg',
                 'img/8.jpg',
                 'img/9.jpg',
-                'img/10.jpg'
+                'img/10.jpg',
+                'data/restaurants.json',
+                'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
+                'https://fonts.gstatic.com/s/roboto/v18/KFOlCnqEu92Fr1MmEU9fBBc4AMP6lQ.woff2'
             ]);
         })
     );
