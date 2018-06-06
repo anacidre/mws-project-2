d7678b82dd95b0e1180efd7d2b6dfbf47fca47ad
Ana Cidre <ana@anacidre.com>
2018-06-06 13:23:54 +0200

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+
+gulp.task('minify', () => {
+    return gulp.src('src/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', () => {
+    gulp.start('minify');
+});
